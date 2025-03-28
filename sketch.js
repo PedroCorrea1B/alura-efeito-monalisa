f710f7149e49d01e4a66b11d749f00517d151432
PedroCorrea1B <00001116863698sp@al.educacao.sp.gov.br>
2025-03-27 21:28:20 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(400, 400);
+}
+let olhoX;
+let olhoY;
+
+function draw() {
+  background("pink");
+  fill("orange");
+  circle(200, 200, 300); // rosto
+  fill("red");
+  circle(150, 150, 60); // olho esquerdo
+  circle(250, 150, 60); // olho direito
+  line(150, 315, 250, 315); // boca
+  fill("brown");
+  triangle(200, 311, 170, 220, 220, 220); // nariz
+  line(123, 100, 178, 100); // sobrancelha esquerda
+  line(278, 100, 178, 100); // sobrancelha direita
+  // circle(150,150,10); // pupila esquerda
+  //circle(250,150,10); // pupila direita
+
+  olhoX = map(mouseX, 1, 600, 130, 170);
+  olhoY = map(mouseY, 1, 600, 130, 170);
+
+  circle(olhoX, olhoY, 25); // nova pupila esquerda
+  circle(olhoX + 100, olhoY, 25); //nova pupila direita
+  if (mouseIsPressed) {
+    console.log(mouseX, mouseY);
+  }
+}
